920cde68511ae25c4027887f7569dcf6b919fe34
ungdungaidn <ungdungaidn@gmail.com>
2025-03-09 21:59:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,3 +92,4 @@ if prompt := st.chat_input("Bạn nhập nội dung cần trao đổi ở đây 
 
     # Cập nhật lịch sử tin nhắn trong session
     st.session_state.messages.append({"role": "assistant", "content": response})
+# Xin Chào Tôi Là Duyet Nguyen AI!
